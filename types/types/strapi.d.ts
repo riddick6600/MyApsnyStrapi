0eb98ca1194e43123f57b7878c3619e6e7096dd9
Георгий Дерябин <riddick6600@mail.ru>
2025-01-21 13:22:36 +0300

    hotel almost

--- types/types/strapi.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '@strapi/strapi' {
-    export type ContentType = string;
-    export const factories: {
-      createCoreController: (uid: ContentType) => any;
-      createCoreRouter: (uid: ContentType) => any;
-      createCoreService: (uid: ContentType) => any;
-    };
-  }
