d0fb4dabafe41cfe9a492fcd494b47d3730ad372
Георгий Дерябин <riddick6600@mail.ru>
2025-01-29 23:43:28 +0300

    work

--- src/admin/app.tsx
+++ src/admin/app.tsx
@@ -2,13 +2,13 @@ import type { StrapiApp } from "@strapi/strapi/admin";
 import ru from "./extensions/translations/ru.json";
 
 export default {
-    config: {
-        locales: ["ru"],
-        translations: {
-            ru: ru,
-        },
+  config: {
+    locales: ["ru"],
+    translations: {
+      ru: ru,
     },
-    bootstrap(app: StrapiApp) {
-        console.log(app);
-    },
+  },
+  bootstrap(app: StrapiApp) {
+    console.log(app);
+  },
 };
